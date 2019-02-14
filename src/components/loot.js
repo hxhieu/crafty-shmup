18aa6ef1d924390335275d1121e00fc7c6ab0d9a
Hugh Hoang <hugh.hoang@gmail.com>
2019-02-14 21:09:55 +1300

    fix: use timer for loot check because HitOn is not reliable since it only triggers once

--- src/components/loot.js
+++ src/components/loot.js
@@ -3,8 +3,9 @@ import '@/components/two-dee-ext'
 import { CollisionProfiles, Events } from '@/constants'
 
 // Local vars
-
+const checkInterval = 250
 const data = new WeakMap()
+const timer = new WeakMap()
 
 // Component definition
 
@@ -13,44 +14,32 @@ Crafty.c('Loot', {
 
   setLootData (loot) {
     data.set(this, loot)
+    timer.set(this, setInterval(() => {
+      const hits = Crafty('Looter').get()
+      for (let i = 0; i < hits.length; i++) {
+        const other = hits[i]
+        if (other.has('Looter') && this.intersect(other.pos())) {
+          const { sound, id, value, volume } = data.get(this)
+          if (sound) {
+            Crafty.audio.play(sound, 1, volume || 1)
+          }
+          other.trigger(Events.LOOT_ACQUIRED, { id, value })
+          this.destroy()
+        }
+      }
+    }, checkInterval))
     return this
   },
 
-  setLootParent (entity) {
-    const { x, y } = entity.getCentrePos()
+  setLootParent (parent) {
+    const { x, y } = parent.getCentrePos()
     this.attr({ x: x + this.w / 2, y: y + this.h / 2 })
+    return this
   },
 
   events: {
-    HitOn,
-    HitOff
-  }
-})
-
-// Helpers
-
-function giveLoot (other) {
-  const { sound, id, value, volume } = data.get(this)
-  if (sound) {
-    Crafty.audio.play(sound, 1, volume || 1)
-  }
-  other.trigger(Events.LOOT_ACQUIRED, { id, value })
-  this.destroy()
-}
-
-function HitOn (hitData) {
-  const other = hitData[0].obj
-  if (other && other.has('Looter')) {
-    giveLoot.call(this, other)
-  }
-}
-
-function HitOff (profile) {
-  const hits = Crafty(profile).get()
-  for (let i = 0; i < hits.length; i++) {
-    const other = hits[i]
-    if (other.has('Looter') && this.intersect(other.pos())) {
-      giveLoot.call(this, other)
+    Remove: function () {
+      clearInterval(timer.get(this))
     }
   }
-}
+})
